e3be8e8322e5b79baa694b40f939412767999a56
James Rowe <jamesrowe@outlook.com>
2020-08-09 19:35:47 -0400

    heroku db

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "12345",
-  database: "burgers_db",
+  host: "ec2-35-153-12-59.compute-1.amazonaws.com",
+  port: 5432,
+  user: "celfcrvumaqasb",
+  password: "1c2840aee57bd45ad914ac88d426ad746f18e40b2272a0c90696425631256d2b",
+  database: "d7ffg4e0v4ahai",
 });
 
 // Make connection.
