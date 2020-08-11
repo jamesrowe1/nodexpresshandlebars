396ea8ecc926a20d0eb84efb15ecda0afc42c69c
James Rowe <jamesrowe@outlook.com>
2020-08-11 15:22:24 -0400

    correct port

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ var mysql = require("mysql");
 //connection info given by heroku jawsdb
 var connection = mysql.createConnection({
   host: process.env.HOST,
-  port: process.env.PORT,
+  port: process.env.DB_PORT,
   user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
